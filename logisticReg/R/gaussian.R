22ca7b9f0330cde0f248655c2dfc53ce9beed015
klin1234@gmail.com <klin1234@gmail.com>
2019-03-28 00:56:42 -0400

    Adding documentation

--- logisticReg/R/gaussian.R
+++ logisticReg/R/gaussian.R
@@ -9,7 +9,7 @@ gaussian_check <- function(X, prob_vec = c(0.5, 1)){
   d <- nrow(X)
 
   diff_vec <- apply(X, 2, function(x){
-    stats::ks.test(x, pnorm, alternative = "two.sided")$statistic
+    stats::ks.test(x, stats::pnorm, alternative = "two.sided")$statistic
   })
 
   quantile(diff_vec, probs = prob_vec)
